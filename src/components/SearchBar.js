f3b94873b76936f067461b5387df839cc0fa4a25
Joshua <joshuaaragon11@gmail.com>
2018-11-22 10:43:38 -0800

    Update SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -13,7 +13,7 @@ class SearchBar extends Component {
     return (
       <div className='container'>
         <label className='display-1 ' style={{maxWidth: '40vw'}}>{this.props.label}</label>
-          <form onSubmit={this.onFormSubmit}> 
+          <form className='form-control' onSubmit={this.onFormSubmit}> 
             <input 
             className = 'form-control'
             placeholder="Enter text.."
@@ -21,8 +21,8 @@ class SearchBar extends Component {
             value={this.state.term}
             onChange={e => this.setState({ term: e.target.value})}
             />
-            <div className='line'/>
           </form>
+          <div className='line'/>
       </div>
     )
   }
